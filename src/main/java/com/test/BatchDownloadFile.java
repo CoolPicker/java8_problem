92ef367b6600e9ff3dd5b9595194860b0f3e275d
lab <niuya312@163.com>
2021-01-22 21:15:44 +0800

    add jvm

--- src/main/java/com/test/BatchDownloadFile.java
+++ src/main/java/com/test/BatchDownloadFile.java
@@ -22,35 +22,43 @@ public class BatchDownloadFile {
 
     public static void main(String[] args) {
         try {
-            BufferedReader bf = new BufferedReader(new FileReader("/home/lab/video_tt.txt"));
+            BufferedReader bf = new BufferedReader(new FileReader("/home/lab/video-tag.log"));
             int i = 1;
             while (true) {
                 String url = bf.readLine();
+                long a = System.currentTimeMillis();
+                downloadByUrl(url,i);
+                long b = System.currentTimeMillis();
+                System.out.println(i + " --- " + url + " --- " + Math.round((b-a)/1000) + " ms");
                 i++;
-                if (i > 38408 && i <= 120000) {
-                    long a = System.currentTimeMillis();
-                    downloadByUrl(url);
-                    long b = System.currentTimeMillis();
-                    System.out.println(i + " --- " + url + " --- " + Math.round((b-a)/1000) + " ms");
-                } else if (i > 120000) break;
+//                if (i > 38408 && i <= 120000) {
+//                    long a = System.currentTimeMillis();
+//                    downloadByUrl(url);
+//                    long b = System.currentTimeMillis();
+//                    System.out.println(i + " --- " + url + " --- " + Math.round((b-a)/1000) + " ms");
+//                } else if (i > 120000) break;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static String downloadByUrl(String url) {
+    private static String downloadByUrl(String item,int index) {
         File file = null;
         String path = "";
         try {
-            if (StringUtils.isNotBlank(url)) {
+            if (StringUtils.isNotBlank(item)) {
+                String[] strings = item.split("\t");
+                String url = strings[1];
+                String tag = strings[2];
+                String tagItem = tag.replace(",", "-");
                 URL remote = new URL(url);
                 HttpURLConnection conn = (HttpURLConnection) remote.openConnection();
                 if (conn.getResponseCode() / 100 != 2) {
                     System.out.println("url connect timeout url:"+url);
                 } else {
                     InputStream videoInputStream = conn.getInputStream();
-                    file = new File("/data/share/20200803/" + getRandomFileName() + ".mp4");
+                    file = new File("/data/test-video/" + tagItem.trim()+"-"+index + ".mp4");
                     FileUtils.copyInputStreamToFile(videoInputStream,file);
                     if (!file.exists()) {
                         System.out.println("url is illegal url:" + url);
